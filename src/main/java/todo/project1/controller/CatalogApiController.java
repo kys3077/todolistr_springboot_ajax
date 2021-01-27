a806f1f9cfb35a8cbe8baa747ee93782cb494cb3
DESKTOP-1QVMQJJ\JKcore <kwony3077@gmail.com>
2021-01-27 10:50:16 +0900

    REST API 적용

--- src/main/java/todo/project1/controller/CatalogApiController.java
+++ src/main/java/todo/project1/controller/CatalogApiController.java
@@ -7,6 +7,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import todo.project1.domain.Catalog;
 import todo.project1.repository.RealJpaCatalogRepository;
@@ -32,21 +34,18 @@ public class CatalogApiController {
     }
 
     @PostMapping("/")
-    public Page<Catalog> create(String title, String content, @PageableDefault(size = 5) Pageable pageable) {
+    public ResponseEntity create(String title, String content, @PageableDefault(size = 5) Pageable pageable) {
         Catalog catalog = new Catalog();
         catalog.setTitle(title);
         catalog.setContent(content);
         catalog.setThis_date(LocalDate.now());
 
         catalogService.register(catalog);
-
-        Page<Catalog> catalogs = realJpaCatalogRepository.findAll(pageable);
-
-        return catalogs;
+        return new ResponseEntity<>("success", HttpStatus.OK);
     }
 
     @PutMapping("/{catalogId}")
-    public Page<Catalog> updateCatalog(@PathVariable("catalogId") Long catalogId, String title, String content
+    public ResponseEntity updateCatalog(@PathVariable("catalogId") Long catalogId, String title, String content
             , @PageableDefault(size = 5) Pageable pageable) {
 
         Catalog catalog = new Catalog();
@@ -57,14 +56,12 @@ public class CatalogApiController {
 
         catalogService.register(catalog);
 
-        Page<Catalog> catalogs = realJpaCatalogRepository.findAll(pageable);
-
-        return catalogs;
+        return new ResponseEntity<>("success", HttpStatus.OK);
     }
 
 
     @DeleteMapping("/{catalogId}")
-    public Page<Catalog> deleteCatalog(@PathVariable("catalogId") Long catalogId, @PageableDefault(size = 5) Pageable pageable) {
+    public ResponseEntity deleteCatalog(@PathVariable("catalogId") Long catalogId, @PageableDefault(size = 5) Pageable pageable) {
         Catalog catalog = new Catalog();
         catalog.setTitle(catalogService.findOne(catalogId).getTitle());
         catalog.setContent(catalogService.findOne(catalogId).getContent());
@@ -73,10 +70,7 @@ public class CatalogApiController {
 
         catalogService.delete(catalog);
 
-
-        Page<Catalog> catalogs = realJpaCatalogRepository.findAll(pageable);
-
-        return catalogs;
+        return new ResponseEntity<>("success", HttpStatus.OK);
     }
 
 }
