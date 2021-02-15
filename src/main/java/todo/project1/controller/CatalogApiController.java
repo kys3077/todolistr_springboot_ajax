bb0e9ed8a4f83cd8d31af63cc703bf112995dce7
DESKTOP-1QVMQJJ\JKcore <kwony3077@gmail.com>
2021-02-15 14:34:11 +0900

    마이바티스 CRUD create만 안됨

--- src/main/java/todo/project1/controller/CatalogApiController.java
+++ src/main/java/todo/project1/controller/CatalogApiController.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.apache.ibatis.annotations.*;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import todo.project1.domain.Catalog;
+import todo.project1.mapper.CatalogMapper;
 import todo.project1.repository.RealJpaCatalogRepository;
 import todo.project1.service.CatalogService;
 
@@ -28,80 +30,103 @@ import java.util.Map;
 @org.springframework.web.bind.annotation.RestController
 @Getter
 @Setter
-@RequiredArgsConstructor
 public class CatalogApiController {
 
     enum CRUD {
         CREATE, READ, UPDATE, DELETE
     }
 
-    private final CatalogService catalogService;
-    private final RealJpaCatalogRepository realJpaCatalogRepository;
+    private CatalogMapper mapper;
 
-    @GetMapping("/read")
-    public List<Catalog> showAllList() {
-        return catalogService.findCatalog();
+    public CatalogApiController(CatalogMapper mapper) {
+        this.mapper = mapper;
     }
 
-    @GetMapping("/list")
-    public Page<Catalog> showList(@RequestParam("page")int page, @RequestParam("size")int size) {
-        System.out.println("page = " + page + "size = " + size);
-        Pageable pageable = PageRequest.of(page, size);
-        Page<Catalog> catalogs = realJpaCatalogRepository.findAll(pageable);
-        return catalogs;
+    @GetMapping("/read")
+    public List<Catalog> getCatalogList(){
+        return mapper.getCatalogList();
     }
 
     @PostMapping("/create")
-    public ResponseEntity create(@RequestBody String model) {
-
-        JSONObject jsonObject = stringToJson(model, CRUD.CREATE);
-        System.out.println(jsonObject);
-
-        Catalog catalog = new Catalog();
-        catalog.setTitle((String) jsonObject.get("title"));
-        catalog.setContent((String) jsonObject.get("content"));
-        catalog.setThis_date(LocalDate.now());
-
-        catalogService.register(catalog);
-        return new ResponseEntity<>("create success", HttpStatus.OK);
+    int insertCatalog(@RequestParam("id")String id, @RequestParam("title") String title, @RequestParam("content") String content, @RequestParam("this_date") String this_date){
+        System.out.println();
+        return mapper.insertCatalog(id, title, content, this_date);
     }
 
     @PostMapping("/update")
-    public ResponseEntity updateCatalog(@RequestBody String model) {
-        JSONObject jsonObject = stringToJson(model, CRUD.UPDATE);
-        System.out.println(jsonObject);
-
-        Long id = Long.valueOf((int) jsonObject.get("id"));//이렇게하는 이유는 자바에서 (LONG) 지원안함
-
-        System.out.println("id@@@@  = " + id);
-        Catalog catalog = new Catalog();
-        catalog.setId(id);
-        catalog.setTitle((String) jsonObject.get("title"));
-        catalog.setContent((String) jsonObject.get("content"));
-        catalog.setThis_date(catalogService.findOne(catalog.getId()).getThis_date());
-
-
-        catalogService.register(catalog);
-
-        return new ResponseEntity<>("update success", HttpStatus.OK);
+    int updateCatalog(@RequestParam("id") String id, @RequestParam("title") String title, @RequestParam("content") String content, @RequestParam("this_date") String this_date){
+        return mapper.updateCatalog(id, title, content, this_date);
     }
-
 
     @DeleteMapping("/delete")
-    public ResponseEntity deleteCatalog(@RequestBody String model) {
-        JSONObject jsonObject = stringToJson(model, CRUD.DELETE);
-        Long id = Long.valueOf((int) jsonObject.get("id"));//이렇게하는 이유는 자바에서 (LONG) 지원안함
-
-        Catalog catalog = new Catalog();
-        catalog.setId(id);
-
-        catalogService.delete(catalog);
-
-        return new ResponseEntity<>("delete success", HttpStatus.OK);
+    int deleteCatalog(@RequestParam("id") String id){
+        return mapper.deleteCatalog(id);
     }
 
-    public JSONObject stringToJson(String model, CRUD crud) {
-        JSONObject jsonObject = new JSONObject(model);
-        return jsonObject;
-    }
+//    @GetMapping("/read")
+//    public List<Catalog> showAllList() {
+//        return catalogService.findCatalog();
+//    }
+//
+//    @GetMapping("/list")
+//    public Page<Catalog> showList(@RequestParam("page")int page, @RequestParam("size")int size) {
+//        System.out.println("page = " + page + "size = " + size);
+//        Pageable pageable = PageRequest.of(page, size);
+//        Page<Catalog> catalogs = realJpaCatalogRepository.findAll(pageable);
+//        return catalogs;
+//    }
+//
+//    @PostMapping("/create")
+//    public ResponseEntity create(@RequestBody String model) {
+//
+//        JSONObject jsonObject = stringToJson(model, CRUD.CREATE);
+//        System.out.println(jsonObject);
+//
+//        Catalog catalog = new Catalog();
+//        catalog.setTitle((String) jsonObject.get("title"));
+//        catalog.setContent((String) jsonObject.get("content"));
+//        catalog.setThis_date(LocalDate.now());
+//
+//        catalogService.register(catalog);
+//        return new ResponseEntity<>("create success", HttpStatus.OK);
+//    }
+//
+//    @PostMapping("/update")
+//    public ResponseEntity updateCatalog(@RequestBody String model) {
+//        JSONObject jsonObject = stringToJson(model, CRUD.UPDATE);
+//        System.out.println(jsonObject);
+//
+//        Long id = Long.valueOf((int) jsonObject.get("id"));//이렇게하는 이유는 자바에서 (LONG) 지원안함
+//
+//        System.out.println("id@@@@  = " + id);
+//        Catalog catalog = new Catalog();
+//        catalog.setId(id);
+//        catalog.setTitle((String) jsonObject.get("title"));
+//        catalog.setContent((String) jsonObject.get("content"));
+//        catalog.setThis_date(catalogService.findOne(catalog.getId()).getThis_date());
+//
+//
+//        catalogService.register(catalog);
+//
+//        return new ResponseEntity<>("update success", HttpStatus.OK);
+//    }
+//
+//
+//    @DeleteMapping("/delete")
+//    public ResponseEntity deleteCatalog(@RequestBody String model) {
+//        JSONObject jsonObject = stringToJson(model, CRUD.DELETE);
+//        Long id = Long.valueOf((int) jsonObject.get("id"));//이렇게하는 이유는 자바에서 (LONG) 지원안함
+//
+//        Catalog catalog = new Catalog();
+//        catalog.setId(id);
+//
+//        catalogService.delete(catalog);
+//
+//        return new ResponseEntity<>("delete success", HttpStatus.OK);
+//    }
+//
+//    public JSONObject stringToJson(String model, CRUD crud) {
+//        JSONObject jsonObject = new JSONObject(model);
+//        return jsonObject;
+//    }
 }
--- src/main/java/todo/project1/mapper/CatalogMapper.java
+++ src/main/java/todo/project1/mapper/CatalogMapper.java
@@ -7,18 +7,18 @@ import java.util.List;
 
 @Mapper
 public interface CatalogMapper {
-    @Select("SELECT * FROM CATALOG WHERE id=${id}")
+    @Select("SELECT * FROM CATALOG WHERE id=#{id}")
     Catalog getCatalog(@Param("id") String id);
 
     @Select("SELECT * FROM CATALOG")
     List<Catalog> getCatalogList();
 
-    @Insert("INSERT INTO CATALOG VALUES(${id}, ${title}, ${content}, ${this_date}")
-    int insertCatalog(@Param("id")String id, @Param("title") String title, @Param("content") String phone, @Param("this_date") String this_date);
+    @Insert("INSERT INTO CATALOG VALUES(#{id}, #{title}, #{content}, #{this_date}")
+    int insertCatalog(@Param("id") String id, @Param("title") String title, @Param("content") String content, @Param("this_date") String this_date);
 
-    @Update("UPDATE CATALOG SET title=${title}, content=${content}, this_date=${this_date} WHERE id=${id}")
-    int updateCatalog(@Param("id") String id, @Param("title") String title, @Param("content") String phone, @Param("this_date") String this_date);
+    @Update("UPDATE CATALOG SET title=#{title}, content=#{content}, this_date=#{this_date} WHERE id=#{id}")
+    int updateCatalog(@Param("id") String id, @Param("title") String title, @Param("content") String content, @Param("this_date") String this_date);
 
-    @Delete("DELETE FROM CATALOG WHERE id=${id}")
+    @Delete("DELETE FROM CATALOG WHERE id=#{id}")
     int deleteCatalog(@Param("id") String id);
 }
